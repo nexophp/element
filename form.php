1b9633ca6f9e4d31a7b4129c2cf0ef6ba5ff4d37
ken <68103403@qq.com>
2025-07-17 16:31:01 +0800

    MIT

--- form.php
+++ form.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * 表单类
+ * @author sunkangchina <68103403@qq.com>
+ * @license MIT <https://mit-license.org/>
+ * @date 2025
+ */
+
 namespace element;
 
 class form
@@ -54,8 +61,8 @@ class form
             include $file;
             $content = ob_get_contents();
             ob_end_clean();
-        }else{
-            return $v['html']??'';
+        } else {
+            return $v['html'] ?? '';
         }
         return $content;
     }
