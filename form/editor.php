725942a12fa01541a5f024f403574f96bb5f5bab
仰望星空 <66815206+sunkangchina@users.noreply.github.com>
2025-07-25 10:20:13 +0800

    editor

--- form/editor.php
+++ form/editor.php
@@ -1,59 +1,15 @@
-<el-form-item label="<?= $label ?>" <?= $item_attr ?>>
-    <textarea id="editor_<?= $name ?>" name="<?= $model ?>[<?= $name ?>]" placeholder=""><?= $value ?></textarea>
+<el-form-item label="<?=$label?>" <?=$item_attr?>>
+    <?=$vue->editor($name)?>
 </el-form-item>
 
+
+
 <?php
-$toolbar = [
-    'undo',
-    'redo',
-    '|',
-    'bold',
-    'italic',
-    '|',
-    'fontColor',
-    'fontBackgroundColor',
-    'insertImage',
-    'insertTable',
-];
-$language = 'zh-cn';
+$vue->editor_method();
+$vue->mounted('we', "
+setTimeout(function(){
+   app.weditor();
+},500);
+");
+
 ?>
-
-<script type="importmap">
-    {
-        "imports": {
-            "ckeditor5": "/misc/ckeditor5/ckeditor5.js",
-            "ckeditor5/": "/misc/ckeditor5/"
-        }
-    }
-</script>
-
-<script type="module">
-    import {
-        ClassicEditor,
-        Essentials,
-        Paragraph,
-        Bold,
-        Italic,
-        Image,
-        ImageInsert,
-        Table,
-        TableToolbar,
-        FileRepository,
-        Font
-    } from 'ckeditor5';
-    ClassicEditor
-        .create(document.querySelector('#editor_<?= $name ?>'), {
-            plugins: [Essentials, Paragraph, Bold, Italic, Font, Image, ImageInsert, Table, TableToolbar, FileRepository],
-            toolbar: <?= json_encode($toolbar) ?>,
-            language: '<?= $language ?>',
-        })
-        .then(editor => {
-            editor.plugins.get('FileRepository').createUploadAdapter = (loader) => {
-                return new MyUploadAdapter(loader);
-            };
-            window.editor_<?= $name ?> = editor;
-        })
-        .catch(error => {
-            console.error(error);
-        });
-</script>
