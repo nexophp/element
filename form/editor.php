9b34bb3a65544a7a64d651f5736aa8c407f6d27a
仰望星空 <66815206+sunkangchina@users.noreply.github.com>
2025-07-27 16:02:25 +0800

    Update editor.php

--- form/editor.php
+++ form/editor.php
@@ -2,20 +2,4 @@
     <?=$vue->editor($name)?>
 </el-form-item>
 
-
-
-<?php
-$vue->editorMethod();
-$vue->mounted('editor_mounted', " 
-if (document.querySelector('#".$name."weditor')) {
-    this.open_editor();
-}
-");
-$vue->method("open_editor()", "
-    setTimeout(function(){
-        if (typeof app !== 'undefined' && app.weditor) {
-            app.weditor();
-        } 
-    }, 300);
-");
-?>
+ 
