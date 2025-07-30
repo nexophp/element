1a8a8ee3aad9094cf8c1bc94a7ee538b4d9eee4d
仰望星空 <66815206+sunkangchina@users.noreply.github.com>
2025-07-30 21:01:16 +0800

    Update attribute.php

--- form/attribute.php
+++ form/attribute.php
@@ -9,40 +9,38 @@
             </tr>
         </thead>
         <tbody>
-            <tr v-for="(v,index) in <?=$model?:'form'?>.<?=$name?>">
+            <tr v-for="(v,index) in <?=$model?:'form'?>.<?=$name?>" :key="index">
                 <td>
-                    <el-input placeholder="<?=lang('属性名如：温度')?>" v-model="<?=$model?:'form'?>.<?=$name?>[index].name"></el-input>
+                    <el-input placeholder="<?=lang('属性名如：温度')?>" v-model="v.name"></el-input>
                 </td>
                 <td>
                     <el-input placeholder="<?=lang('多值用,、或空格分隔如：标准冰、少冰、去冰、温、热')?>"
-                        v-model="<?=$model?:'form'?>.<?=$name?>[index].values"></el-input>
+                        v-model="v.values"></el-input>
                 </td>
                 <td>
                     <el-button type="danger" size="small" @click="form_del_attr('<?=$name?>',index)"
                         icon="el-icon-delete" circle></el-button>
                 </td>
             </tr>
-
         </tbody>
     </table>
 </el-form-item>
 <?php 
-$default = "[
-	{name:'',values:''}, 
-]";
-$vue->formData($name,"");
+$default = "[{name:'',values:''}]";
+$vue->formData($name, $default);
  
 $vue->method("form_push_attr(field)","
-	if(!this.".$model."[field]){
-		this.".$model."[field] = ".$default.";
-	} 
-	this.".$model."[field].push({name:'',values:''});
-	this.\$forceUpdate();
+    if(!this.".$model."[field]) {
+        this.\$set(this.".$model.", field, ".$default.");
+    }
+    this.".$model."[field].push({name:'',values:''});
 ");
+
 $vue->method("form_del_attr(field,index)","
-	this.".$model."[field].splice(index,1);
+    this.".$model."[field].splice(index,1);
 ");
+
 $vue->method("load_attr()","
-	this.\$set(this.".$model.",'".$name."',".$default."); 
+    this.\$set(this.".$model.",'".$name."',".$default.");
 ");
 ?>
