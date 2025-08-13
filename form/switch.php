93151f2bc6b8598f780f596a1c3e8002f6e8e9c1
ken <68103403@qq.com>
2025-08-13 15:52:39 +0800

    switch

---
+++ form/switch.php
@@ -0,0 +1,5 @@
+<el-form-item label="<?=$label?>" <?=$item_attr?>>
+    <el-switch class="table_input" v-model="<?=$model?>.<?=$name?>" <?=$attr_element?>>
+       
+    </el-switch>
+</el-form-item>
