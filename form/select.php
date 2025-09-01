6740cba5b3019918f3d4a0ea15079537c76c53d9
ken <68103403@qq.com>
2025-09-01 17:43:39 +0800

    select 支持value是vue变量

--- form/select.php
+++ form/select.php
@@ -1,13 +1,25 @@
-<el-form-item label="<?=$label?>" <?=$item_attr?>>
-    <el-select class="table_input" v-model="<?=$model?>.<?=$name?>" <?=$attr_element?>>
+<el-form-item label="<?= $label ?>" <?= $item_attr ?>>
+    <el-select class="table_input" v-model="<?= $model ?>.<?= $name ?>" <?= $attr_element ?>>
         <?php
-    if($v['value'] && is_array($v['value'])) {
-        foreach($v['value'] as $kk => $vv) {?>
-        <el-option label="<?=$vv['label']?>" value="<?=$vv['value']?>"></el-option>
-        <?php }
-        }?>
-        <?php if($v['inner']) {
+        if ($v['value']) {
+            if (is_array($v['value'])) {
+                foreach ($v['value'] as $kk => $vv) { ?>
+                    <el-option label="<?= $vv['label'] ?>" value="<?= $vv['value'] ?>"></el-option>
+                <?php }
+            } else { ?>
+                <el-option
+                    v-for="item in <?= $v['value'] ?>"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value">
+                </el-option>
+        <?php
+            }
+        }
+        ?>
+        <?php if ($v['inner']) {
             echo $v['inner'];
-        }?>
+        }
+        ?>
     </el-select>
 </el-form-item>
